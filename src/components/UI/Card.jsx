18d7bd88ac0dd5f6da4edec91536e175f3db7ffb
mdallamaiqbal <bd999plus13@gmail.com>
2026-04-16 13:36:59 +0600

    add friend card when i add data json conect with friend card to card section and i creat custom hooks and also a loader

---
+++ src/components/UI/Card.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Card = ({card}) => {
+return (
+<div>
+    <div className='p-6 rounded-lg transition duration-300 hover:shadow-lg bg-white shadow text-center'>
+        <img src={card.picture} className='mx-auto transition duration-300 hover:scale-110' alt="" />
+        <h3 className='text-2xl font-semibold mt-4'>{card.name}</h3>
+        <p className='text-sm text-[#64748B]'>{card.days_since_contact}d ago</p>
+        <div className='flex gap-2 my-2 justify-center'>
+            {
+            card.tags?.map((tag,ind)=> <p className='badge text-sm bg-[#CBFADB] text-[#244D3F]' key={ind}>{tag}</p>)
+            }
+        </div>
+        <p className={`badge text-white ${card.status==='Almost Due' &&'bg-[#EFAD44]'} ${card.status==='Overdue'
+            &&'bg-[#EF4444]'} ${card.status==='On-Track' &&'bg-[#244D3F]'} `}>{card.status}</p>
+    </div>
+</div>
+);
+};
+
+export default Card;
---
+++ src/components/hooks/useFriendHooks.jsx
@@ -0,0 +1,19 @@
+import React, { useEffect, useState } from 'react';
+
+const useFriendHooks = () => {
+     const [cards,setCards] = useState([]);
+    const [loading,setLoading]=useState(true)
+    useEffect(()=>{
+       const fetchData =async ()=>{
+       const res = await fetch('./data.json');
+       const data = await res.json();
+       setTimeout(()=>{
+         setCards(data);
+         setLoading(false)
+       },500)}
+       fetchData()
+    },[])
+    return {cards,loading}
+};
+
+export default useFriendHooks;
